dbe3511e90f8249d01f036da95217688295e7a51
Ray Hernandez <hernan43@gmail.com>
2025-02-22 15:46:11 -0500

    Add some timing tweaks for my personal taste

--- matrix_display.py
+++ matrix_display.py
@@ -22,23 +22,40 @@ GIF_DIR = "/opt/gifs"
 current_gif = None
 stop_event = threading.Event()
 
-def display_gif(gif_path):
+def display_gif(gif_path, frame_delay=0.10, forced_loops=2):
     """Displays a GIF frame by frame on the LED matrix."""
     global current_gif
     current_gif = os.path.basename(gif_path)
 
     try:
         with Image.open(gif_path) as gif:
-            for frame in ImageSequence.Iterator(gif):
+
+            gif_loop_count = gif.info.get("loop", 1)
+            if gif_loop_count == 0:
+                gif_loop_count = forced_loops
+
+            if gif.n_frames < 8:
+                # add more if it is a short one
+                gif_loop_count += forced_loops
+
+            for x in range(gif_loop_count):
+                print(f"Playing {gif_path}({gif.n_frames}) count {x+1}")
+
                 if stop_event.is_set():
                     return
-                frame = frame.convert("RGBA").resize((matrix.width, matrix.height))
-                black_bg = Image.new("RGB", frame.size, (0, 0, 0))
-                frame = Image.alpha_composite(black_bg.convert("RGBA"), frame).convert("RGB")
-                matrix.SetImage(frame)
-                time.sleep(0.1)
 
-        #clear_matrix()
+                for frame in ImageSequence.Iterator(gif):
+                    if stop_event.is_set():
+                        return
+                    frame = frame.convert("RGBA").resize((matrix.width, matrix.height))
+                    black_bg = Image.new("RGB", frame.size, (0, 0, 0))
+                    frame = Image.alpha_composite(black_bg.convert("RGBA"), frame).convert("RGB")
+                    matrix.SetImage(frame)
+                    time.sleep(frame_delay)
+
+                    if stop_event.is_set():
+                        return
+
 
     except Exception as e:
         print(f"Error displaying {gif_path}: {e}")
@@ -49,7 +66,7 @@ def clear_matrix(delay=0.5):
     matrix.SetImage(black_image)
     time.sleep(delay)
 
-def display_time(interval=15):
+def display_time(interval=10):
     """Displays the current time on the matrix."""
     font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
     font = ImageFont.truetype(font_path, 24)
